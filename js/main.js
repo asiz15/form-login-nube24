28baa7ccbd40236b49c08aecc2eb5a21b23a8950
Alex Silva Z <a.silvazapata@gmail.com>
2017-11-13 03:08:05 -0300

    Nube24 v1.0
    v1.0

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+	
+	  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
+
+$('.type-it').typeIt({
+    speed: 60,
+    lifeLike: false,
+    autoStart: false
+  })
+
+
+
+
+
+
+
+	}); /* jquery ready*/
